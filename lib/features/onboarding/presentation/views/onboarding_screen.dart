cbbd742f8825bc23f6f859b5f707dee58638b5e6
RottenShadow <xlostunite@gmail.com>
2025-11-24 22:43:12 +0200

    fixed empty login screen bug after logout

--- lib/features/onboarding/presentation/views/onboarding_screen.dart
+++ lib/features/onboarding/presentation/views/onboarding_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:qoot/core/common/widgets/custom_loading.dart';
 import 'package:qoot/core/utils/app_animations.dart';
-import '../../../../core/middlewares/cubit/auth_middleware_cubit.dart';
 import '../../../auth/register_account/presentation/views/auth_initialize.dart';
 import '../logic/cubit/onboarding_cubit.dart';
 
